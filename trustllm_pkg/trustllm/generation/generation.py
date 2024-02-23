304ddb55bebe39a95bea938e88078ed22eb438e4
Allyson Ettinger <allysone@allenai.org>
2024-02-23 13:29:14 -0500

    trace

--- trustllm_pkg/trustllm/generation/generation.py
+++ trustllm_pkg/trustllm/generation/generation.py
@@ -56,6 +56,7 @@ class LLMGeneration:
             """
 
         prompt = self._prompt2conversation(prompt)
+        import pdb; pdb.set_trace()
         inputs = tokenizer([prompt])
         inputs = {k: torch.tensor(v).to(self.device) for k, v in inputs.items()}
         output_ids = model.generate(
